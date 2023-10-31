582d092c5d11d7cea5c84bbb808ce60220e79d84
Peter Brian <wahomebrian98@gmail.com>
2023-10-31 08:09:35 +0300

    update

--- Python_pizza_delivery/pizza_order.py
+++ Python_pizza_delivery/pizza_order.py
@@ -3,7 +3,28 @@
 '''
 This script allows users to order pizzas by selecting the size, adding pepperoni, and choosing extra cheese. It calculates the total bill based on the user's choices.
 '''
-
+print('''
+    And how about a slice?
+                        ___
+                        |  ~~--.
+                        |%=@%%/
+                        |o%%%/
+                     __ |%%o/
+               _,--~~ | |(_/ ._
+            ,/'  m%%%%| |o/ /  `\.
+           /' m%%o(_)%| |/ /o%%m `\
+         /' %%@=%o%%%o|   /(_)o%%% `\
+        /  %o%%%%%=@%%|  /%%o%%@=%%  \
+       |  (_)%(_)%%o%%| /%%%=@(_)%%%  |
+       | %%o%%%%o%%%(_|/%o%%o%%%%o%%% |
+       | %%o%(_)%%%%%o%(_)%%%o%%o%o%% |
+       |  (_)%%=@%(_)%o%o%%(_)%o(_)%  |
+        \ ~%%o%%%%%o%o%=@%%o%%@%%o%~ /
+         \. ~o%%(_)%%%o%(_)%%(_)o~ ,/
+           \_ ~o%=@%(_)%o%%(_)%~ _/
+             `\_~~o%%%o%%%%%~~_/'
+                `--..____,,--'
+''')
 print("Welcome to Python Pizza Deliveries!")
 
 try:
