ae6e5b827917c7fe4ff8b05989cdb3084db939ae
Peter Brian <wahomebrian98@gmail.com>
2023-10-31 07:42:51 +0300

    Python pizza delivery project

---
+++ Python_pizza_delivery/README.md
@@ -0,0 +1,56 @@
+# Python Pizza Deliveries
+
+![Pizza](pizza.jpg)
+
+Welcome to the Python Pizza Deliveries program! This Python script allows users to order pizzas by selecting the size, adding pepperoni, and choosing extra cheese. It calculates the total bill based on the user's choices.
+
+## Table of Contents
+
+- [Features](#features)
+- [Prerequisites](#prerequisites)
+- [Usage](#usage)
+- [License](#license)
+
+## Features
+
+- **Pizza Size Selection**: Users can choose from three pizza sizes: Small (S), Medium (M), and Large (L).
+
+- **Toppings**: Users can add pepperoni and extra cheese to their pizza.
+
+- **Input Validation**: The program validates user inputs to ensure they select valid options (S, M, L, Y, or N) and provides informative error messages for invalid inputs.
+
+- **Bill Calculation**: The script calculates the total bill based on the pizza size and selected toppings.
+
+## Prerequisites
+
+- Python 3.x installed on your system
+
+## Usage
+
+1. Clone the repository or download the Python script.
+
+2. Open a terminal or command prompt.
+
+3. Navigate to the directory containing the script.
+
+4. Run the program using Python 3:
+
+    ```bash
+    python3 pizza_delivery.py
+    ```
+
+5. Follow the on-screen prompts to select the pizza size, choose pepperoni, and add extra cheese.
+
+6. The program will calculate the total bill and display it to the user.
+
+## License
+
+This project is open-source and available under the [MIT License](LICENSE).
+
+## GitHub Username
+
+This project is maintained by [Peter_Mwangi254](https://github.com/Peter_Mwangi254). Feel free to contribute, report issues, or provide feedback.
+
+Enjoy your Python Pizza Deliveries!
+
+![Pizza](pizza.jpg)
---
+++ Python_pizza_delivery/pizza_order.py
@@ -0,0 +1,86 @@
+#!/usr/bin/python3
+
+'''
+This script allows users to order pizzas by selecting the size, adding pepperoni, and choosing extra cheese. It calculates the total bill based on the user's choices.
+'''
+
+print("Welcome to Python Pizza Deliveries!")
+
+try:
+    '''
+    User input for pizza size. The input is processed to remove leading/trailing spaces and convert to uppercase.
+    '''
+
+    size = input("What pizza size do you want? S, M, L? ").strip().upper()
+
+    '''
+    If the selected size is not S, M, or L, a ValueError is raised with an error message.
+    '''
+    if size not in ["S", "M", "L"]:
+        raise ValueError("Invalid size. Please select a valid size (S, M, L).")
+
+    '''
+    User input for adding pepperoni. The input is processed to remove leading/trailing spaces and convert to uppercase.
+    '''
+    add_pepperoni = input("Do you want pepperoni? Y or N?").strip().upper()
+
+    '''
+    If the user's choice for pepperoni is not Y or N, a ValueError is raised with an error message.
+    '''
+    if add_pepperoni not in ["Y", "N"]:
+        raise ValueError("Invalid choice. Please enter Y for Yes or N for No.")
+
+    '''
+    User input for adding extra cheese. The input is processed to remove leading/trailing spaces and convert to uppercase.
+    '''
+    
+    extra_cheese = input("Do you want extra cheese? Y or N?").strip().upper()
+
+    '''
+    If the user's choice for extra cheese is not Y or N, a ValueError is raised with an error message.
+    '''
+
+    if extra_cheese not in ["Y", "N"]:
+        raise ValueError("Invalid choice. Please enter Y for Yes or N for No.")
+
+    bill = 0
+
+
+    '''
+    Calculate the base cost of the pizza based on the selected size.
+    '''
+    if size == "S":
+        bill += 15
+    elif size == "M":
+        bill += 20
+    else:
+        bill +=25
+
+    '''
+    If the user wants pepperoni, additional costs are added based on the pizza size.
+    '''
+
+    if add_pepperoni == "Y":
+        if size == "S":
+            bill += 2
+        else:
+            bill += 3
+
+    '''
+    If the user wants extra cheese, an additional cost of 1 is added to the bill.
+    '''
+
+    if extra_cheese == "Y":
+        bill += 1
+
+    '''
+    Display the final bill to the user.
+    '''
+
+    print(f"Your final bill is ${bill}")
+
+except ValueError as e:
+    '''
+    Handle ValueError exceptions by displaying the error message to the user.
+    '''
+    print(f"Error: {e}")
